97c7dfd9f61b7a347e0273d3a93f5f3d8837ca28
Kendall Burdette <kjburdette2015@gmail.com>
2021-06-07 13:11:37 -0400

    set up routes

--- clubwsb/src/App.js
+++ clubwsb/src/App.js
@@ -1,9 +1,38 @@
 import './App.css';
+import {BrowserRouter as Router, Switch, Route, Link, Redirect} from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import SignUp from './components/SignUp';
+import Error from './components/Error';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Router>
+        <div>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/signup">Sign Up</Link>
+          </nav>
+        </div>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/signup">
+            <SignUp />
+          </Route>
+          <Route path="*">
+            <Error/>
+          </Route>
+        </Switch>
+      </Router>
+      <Footer />
     </div>
   );
 }
