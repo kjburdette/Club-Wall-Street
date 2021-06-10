89070b595a7fbf6cddf133db15f813212276b810
Kendall Burdette <kjburdette2015@gmail.com>
2021-06-10 14:43:51 -0400

    edit home page route

--- clubwsb/src/App.js
+++ clubwsb/src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Router>
         <Nav/>
         <Switch>
-          <Route exact path="/">
+          <Route exact path="/Club-Wall-Street">
             <Home />
             <Footer />
           </Route>
