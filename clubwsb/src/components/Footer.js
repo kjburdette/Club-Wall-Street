f06574f9010c3a7aba7ce50b82b6c8ef15041d16
Kendall Burdette <kjburdette2015@gmail.com>
2021-06-10 15:10:35 -0400

    edited links to open in new tab

--- clubwsb/src/components/Footer.js
+++ clubwsb/src/components/Footer.js
@@ -4,15 +4,15 @@ export default function Footer() {
     return (
         <div className="footer-container">
             <p>©2021 Club Wall Street</p>
-            <p>All game art created by alesta_b, check out her work <a href="https://www.fiverr.com/alesta_b?source=order_page_user_message_link">here.</a></p>
+            <p>All game art created by alesta_b, check out her work <a href="https://www.fiverr.com/alesta_b?source=order_page_user_message_link" alt="" target="_blank" rel="noopener noreferrer">here.</a></p>
             <p>Follow us on social media:</p>
-            <a href="https://www.facebook.com" alt="">
+            <a href="https://www.facebook.com" alt="" target="_blank" rel="noopener noreferrer">
                 <i class="fab fa-facebook"></i>
             </a>
-            <a href="https://www.instagram.com" alt="">
+            <a href="https://www.instagram.com" alt="" target="_blank" rel="noopener noreferrer">
                 <i class="fab fa-instagram"></i>
             </a>
-            <a href="https://www.twitter.com" alt="">
+            <a href="https://www.twitter.com" alt="" target="_blank" rel="noopener noreferrer">
                 <i class="fab fa-twitter"></i>
             </a>
         </div>
